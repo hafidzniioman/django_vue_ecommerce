69010883f797893bfc9798c4f10e818197b90337
hafidzniioman <hafidzniioman@gmail.com>
2020-12-01 17:29:13 +0700

    showing product on frontpage

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from apps.store.models import Product
+
 def frontpage(request):
-    return render(request, 'frontpage.html')
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'frontpage.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
